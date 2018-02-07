689d0b149af797058ee887b5bf6d2ae7dd3ffe1d
Sandstorm <alexis.hubert@student.ecp.fr>
2018-02-07 18:31:27 +0000

    Home button now visible

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var ejs = require('ejs');
 app = express();
 
 app.get('/', (req, res) => res.render('accueil.ejs'));
-app.use('/static', express.static(__dirname + '/views/resources'))
-app.use('/static', express.static(__dirname+'/views/css'))
+app.use('/staticres', express.static(__dirname + '/views/resources'))
+app.use('/staticcss', express.static(__dirname+'/views/css'))
 
 
 
