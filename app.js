95a7550c97bdf9dce25d058145ca702a219d0f4b
Sandstorm <alexis.hubert@student.ecp.fr>
2018-02-07 10:48:05 +0000

    built a working docker :)

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var ejs = require('ejs');
 app = express();
 
+app.get('/', (req, res) => res.render('accueil.ejs'));
 
 
 
 
 
 
-
-
+app.listen(3000, () => console.log('Hello world'));
 app.set('view engine', 'ejs');
