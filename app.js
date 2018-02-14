9ca73185e188eebaca765b16d7e3bcd4d650e1ca
Sandstorm <alexis.hubert@student.ecp.fr>
2018-02-14 10:37:06 +0000

    added routes for accueil. Now working

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-app = express();
+var path = require('path');
+var app = express();
 
-app.get('/', (req, res) => res.render('accueil.ejs'));
+var accueil = require('./routes/route_accueil.js');
+//var resources = require('./routes/route_resources_HP');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//app.get('/', (req, res) => res.render('accueil.ejs'));
 app.use('/staticres', express.static(__dirname + '/views/resources'))
-app.use('/staticcss', express.static(__dirname+'/views/css'))
+app.use('/staticcss', express.static(__dirname + '/views/css'))
 
 
 
-
+app.use('/', accueil);
+//app.use('/resources', resources);
 
 
 app.listen(3000, () => console.log('Hello world'));
-app.set('view engine', 'ejs');
+
+module.exports = app;
