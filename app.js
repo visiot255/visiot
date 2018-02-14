5046f27df73961d2b8562e2219c11a41a63e342d
Sandstorm <alexis.hubert@student.ecp.fr>
2018-02-14 14:50:36 +0000

    cleaned access to css

--- app.js
+++ app.js
@@ -5,19 +5,21 @@ var cookieParser = require('cookie-parser');
 var path = require('path');
 var app = express();
 
+var css = express.static(__dirname + '/public');
+
 var accueil = require('./routes/route_accueil.js');
 var resources = require('./routes/route_resources_HP');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 //app.get('/', (req, res) => res.render('accueil.ejs'));
-app.use('/staticres', express.static(__dirname + '/views/resources'));
-app.use('/staticcss', express.static(__dirname + '/views/css'));
+//app.use('/staticres', express.static(__dirname + '/views/resources'));
+//app.use('/staticcss', express.static(__dirname + '/views/css'));
 
-
+app.use('/', css)
 
 app.use('/', accueil);
-app.use('/resources', resources);
+app.use('/resources/', resources);
 
 
 app.listen(3000, () => console.log('Hello world'));
