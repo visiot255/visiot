565a188db08bf5a7cd1718e66540f776d082e2a8
Sandstorm <alexis.hubert@student.ecp.fr>
2018-02-28 17:10:47 +0100

    end session. Started working on controller

--- controller/controller_add_articles.js
+++ controller/controller_add_articles.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var config = require('../config');
+var Article = require('../models/models');
 
 
 const createNew = function(req, res){
 
+    var title = req.body.post_title;
+    var content = req.body.post_content;
+    var parent = req.body.post_parent;
+
+    var newArticle = new Article({
+
+    })
 };
 
 module.exports = {createNew};
