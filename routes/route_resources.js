f904a59f892c67d8e541218a9846673dab79417a
space <thierry.tio@student.ecp.fr>
2018-04-21 16:14:14 +0200

    ejs now renders links to parents, children and siblings

--- routes/route_resources.js
+++ routes/route_resources.js
@@ -11,9 +11,14 @@ router.get('/', function(req, res){
 
 router.get('/:id', function(req, res){
   models.article.findOne({where:{title: req.params.id}}).then(article => {
-    var htmlContent = converter.makeHtml(article.content);
-    console.log(htmlContent);
-    res.render('article', {config: config, content: htmlContent})
+    models.article.findOne({where: {title: article.parent}}).then(parent => {
+      models.article.findAll({where: {parent: article.parent}}).then(siblings => {
+        models.article.findAll({where: {parent: article.title}}).then(children => {
+          var htmlContent = converter.makeHtml(article.content);
+          res.render('article', {config: config, content: htmlContent, children: children, parent: parent, siblings:siblings})
+        })
+      })
+    })
   })
 }
 )
