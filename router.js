12fd9b4894534e21f59a394a95e33b2550d2aee0
Sandstorm <alexis.hubert@student.ecp.fr>
2018-05-09 18:10:13 +0200

    Started to work on modfiying articles. Controller isn't working however. Take it back later

--- router.js
+++ router.js
@@ -3,11 +3,13 @@ var resources = require('./routes/route_resources');
 var addArticles = require('./routes/route_add_articles');
 var quiz = require('./routes/route_quiz');
 var aboutus = require('./routes/route_aboutus');
+var modifyArticles = require('./routes/route_modify_articles.js');
 
 module.exports = {
     accueil,
     resources,
     addArticles,
     quiz,
-    aboutus
+    aboutus,
+    modifyArticles
 }
