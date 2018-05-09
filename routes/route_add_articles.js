0de204f51c85137ddd5fb5ac377696fadf43ac02
Sandstorm <alexis.hubert@student.ecp.fr>
2018-05-09 16:35:46 +0200

    Added buttons to add_articles. Started to comment code

--- routes/route_add_articles.js
+++ routes/route_add_articles.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../config');
+var models = require('../models/models');
 var controller = require('../controller/controller_add_articles');
+var Sequelize = require('sequelize');
 
+// Renders normal page where you can add an article.
 router.get('/', function(req,res){
-  res.render('add_articles', {config: config});
+  res.render('add_articles', {config: config, article: undefined});
 });
 
+//Renders page with a parent article title predefined
+//Helps creating articles on a par with an article.
+router.get('/:id', function(req,res){
+  var idconv = req.params.id.split('_').join(' ');
+  models.article.findOne({where: {title: idconv}}).then(article => {
+    console.log(article.parent);
+    res.render('add_articles', {config: config, article: article});
+  })
+});
+
+//Calls controller to add the newly defined article to the databse.
 router.route('/createNew').post(controller.createNew);
 
 module.exports = router;
